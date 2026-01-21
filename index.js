5e096c0da268f179822684c71979edbe8ccaff32
viruk13 <virukinnal04@gmail.com>
2026-01-21 17:03:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./App.css";
 
-// Create root and render App
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
