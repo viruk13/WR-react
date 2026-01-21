6d9449826d8206b81a77c66ab17502f69669662b
viruk13 <virukinnal04@gmail.com>
2026-01-21 17:11:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -31,3 +31,15 @@ function CoreServices() {
 }
 
 export default CoreServices;
+import React from 'react';
+import CoreServices from './CoreServices'; // import the component
+
+function App() {
+  return (
+    <div>
+      <CoreServices />
+    </div>
+  );
+}
+
+export default App;
